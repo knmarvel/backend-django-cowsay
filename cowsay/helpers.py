41c1f37f1de18612b8d70031ce87906be05a29f4
Karen Marvel <knmarvel@gmail.com>
2020-05-13 10:51:24 -0400

    took cowsay from poetry dependency

--- cowsay/helpers.py
+++ cowsay/helpers.py
@@ -4,8 +4,10 @@ def cow_translator(text, cow_version):
     """Takes text and the name of the cow we want to display
     and runs it through the terminal command line and then 
     returns the string from the commandline"""
-    cla = "cowsay -f " + cow_version + " " + text
-    display_text = subprocess.check_output(cla, shell=True)
-    return display_text.decode('utf8')
+    cla = ["cowsay", "-f", cow_version, text]
+    print(" ".join(cla))
+    display_text = subprocess.check_output(cla).decode('utf8')
+    print(display_text)
+    return display_text
 
-print(cow_translator("moo","dragon"))
+cow_translator("hi", "surgery")
