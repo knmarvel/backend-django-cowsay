2a15f56362a12a3deab1b3bad931bb573f8a73eb
Kano Marvel <knmarvel@gmail.com>
2021-01-03 00:22:22 -0500

    made cowsay ubuntu compatable for digitalocean deployment

--- cowsay/helpers.py
+++ cowsay/helpers.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 def cow_translator(text, cow_version):
     """Takes text and the name of the cow we want to display
     and runs it through the terminal command line and then 
@@ -9,5 +10,3 @@ def cow_translator(text, cow_version):
     display_text = subprocess.check_output(cla).decode('utf8')
     print(display_text)
     return display_text
-
-cow_translator("hi", "surgery")
--- cowsay/models.py
+++ cowsay/models.py
@@ -1,60 +1,61 @@
 from django.db import models
-from django.utils import timezone
-from datetime import datetime
+
 
 # Create your models here.
 class InputText(models.Model):
     COW_CHOICES = [
         ("default", "default"),
-        ("beavis.zen","beavis.zen"),
-        ("blowfish","blowfish"),
-        ("bud-frogs","bud-frogs"),
-        ("bunny","bunny"),
-        ("cheese","cheese"),
-        ("cower","cower"),
-        ("daemon","daemon"),
-        ("dragon","dragon"),
-        ("dragon-and-cow","dragon-and-cow"),
-        ("elephant","elephant"),
-        ("elephant-in-snake","elephant-in-snake"),
-        ("eyes","eyes"),
-        ("flaming-sheep","flaming-sheep"),
-        ("ghostbusters","ghostbusters"),
-        ("head-in","head-in"),
-        ("hellokitty","hellokitty"),
-        ("kiss","kiss"),
-        ("kitty","kitty"),
-        ("koala","koala"),
-        ("kosh","kosh"),
-        ("luke-koala","luke-koala"),
-        ("meow","meow"),
-        ("milk","milk"),
-        ("moofasa","moofasa"),
-        ("moose","moose"),
-        ("ren","ren"),
-        ("sheep","sheep"),
-        ("skeleton","skeleton"),
-        ("small","small"),
-        ("stegosaurus","stegosaurus"),
-        ("stimpy","stimpy"),
-        ("supermilker","supermilker"),
-        ("surgery","surgery"),
-        ("telebears","telebears"),
-        ("three-eyes","three-eyes"),
-        ("turkey","turkey"),
-        ("turtle","turtle"),
-        ("tux","tux"),
-        ("udder","udder"),
-        ("vader","vader"),
-        ("vader-koala","vader-koala"),
-        ("www","www")
+        ("bud-frogs", "bud-frogs"),
+        ("bunny", "bunny"),
+        ("calvin", "calvin")
+        ("cheese", "cheese"),
+        ("cower", "cower"),
+        ("daemon", "daemon"),
+        ("dragon", "dragon"),
+        ("dragon-and-cow", "dragon-and-cow"),
+        ("duck", "duck"),
+        ("elephant", "elephant"),
+        ("elephant-in-snake", "elephant-in-snake"),
+        ("eyes", "eyes"),
+        ("flaming-sheep", "flaming-sheep"),
+        ("fox", "fox"),
+        ("ghostbusters", "ghostbusters"),
+        ("gnu", "gnu"),
+        ("hellokitty", "hellokitty"),
+        ("kangaroo", "kangaroo"),
+        ("kiss", "kiss"),
+        ("kitty", "kitty"),
+        ("koala", "koala"),
+        ("kosh", "kosh"),
+        ("luke-koala", "luke-koala"),
+        ("mech-and-cow", "mech-and-cow"),
+        ("milk", "milk"),
+        ("moofasa", "moofasa"),
+        ("moose", "moose"),
+        ("pony", "pony"),
+        ("pony-smaller", "pony-smaller"),
+        ("ren", "ren"),
+        ("sheep", "sheep"),
+        ("skeleton", "skeleton"),
+        ("snowman", "snowman"),
+        ("stegosaurus", "stegosaurus"),
+        ("stimpy", "stimpy"),
+        ("suse", "suse"),
+        ("three-eyes", "three-eyes"),
+        ("turkey", "turkey"),
+        ("turtle", "turtle"),
+        ("tux", "tux"),
+        ("unipony", "unipony"),
+        ("unipony-smaller", "unipony-smaller"),
+        ("vader", "vader"),
+        ("vader-koala", "vader-koala"),
+        ("www", "www")
     ]
     text = models.CharField(max_length=200)
     cow = models.CharField(
         max_length=20,
         choices=COW_CHOICES
     )
-    
 
     def __str__(self):
         return self.text
