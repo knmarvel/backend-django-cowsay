9d85804b8d2663e979efe465813e3262d2c90285
Kano Marvel <knmarvel@gmail.com>
2021-01-03 17:59:10 -0500

    ready for deployment

--- cowsay/helpers.py
+++ cowsay/helpers.py
@@ -6,7 +6,5 @@ def cow_translator(text, cow_version):
     and runs it through the terminal command line and then
     returns the string from the commandline"""
     cla = ["cowsay", "-f", cow_version, text]
-    print(" ".join(cla))
     display_text = subprocess.check_output(cla).decode('utf8')
-    print(display_text)
     return display_text
